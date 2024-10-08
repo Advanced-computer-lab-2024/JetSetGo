04a5a1dc737e2ed026632903492452600f1c2588
MarkMaged01 <magedmark50@gmail.com>
2024-10-08 03:17:22 +0300

    k

--- JetSetGo1/server/controllers/tourGuideController.js
+++ JetSetGo1/server/controllers/tourGuideController.js
@@ -180,21 +180,18 @@ const deleteItinerary = async (req, res) => {
     res.status(400).json({ message: "Error deleting itinerary", error });
   }
 };
+
 //Read My Itineraries
 const showMyItineraries = async(req,res) => {
-   
-
   const guideId = req.query.guideId;
-
   try{
-      const result = await ItineraryModel.find({tourGuide:(guideId)})
+      const result = await Itinerary.find({tourGuide:(guideId)})
       res.status(200).json(result)
     }catch{
       res.status(400).json({error:"Id is required"})
     }
-
-
 }
+
 
 module.exports = {
   createProfile,
